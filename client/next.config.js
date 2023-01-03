6c00ca91bf1bd73308ff3a6e85589cef91417a16
JM <59332514+mjosh51@users.noreply.github.com>
2023-01-03 13:08:02 +0100

    Add files via upload

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,21 @@ const nextConfig = {
   swcMinify: true,
   eslint: { ignoreDuringBuilds: true },
   env: {
-    API_URI: 'http://localhost:8080/api',
+    POSTS_URL: process.env.POSTS_URL,
+    TIPS_URL: process.env.TIPS_URL,
+    CATEGORIES_URL: process.env.CATEGORIES_URL,
+    USER_DASHBOARD_URL: process.env.USER_DASHBOARD_URL,
+    UPLOAD_IMAGE_URL: process.env.UPLOAD_IMAGE_URL,
+    UPDATE_PROFILE_URL: process.env.UPDATE_PROFILE_URL,
+    UPDATE_URL: process.env.UPDATE_URL,
+    RECOVERY_URL: process.env.RECOVERY_URL,
+    RESET_URL: process.env.RESET_URL,
+    REFRESH_URL: process.env.REFRESH_URL,
+    LOGIN_URL: process.env.LOGIN_URL,
+    LOGOUT_URL: process.env.LOGOUT_URL,
+    ADMIN_URL: process.env.ADMIN_URL,
+    PENDING_URL: process.env.PENDING_URL,
+    REJECTED_URL: process.env.REJECTED_URL,
   },
   images: {
     domains: ['res.cloudinary.com', 'cdn.pixabay.com'],
@@ -12,26 +26,6 @@ const nextConfig = {
   experimental: {
     images: { allowFutureImage: true },
     largePageDataBytes: 128 * 100000,
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/(.*)',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
   },
 };
 
